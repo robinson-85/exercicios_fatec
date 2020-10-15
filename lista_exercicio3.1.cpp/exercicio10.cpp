2867dca5a7f2aae3dfc10d839151908d634752a3
Robinson <robinpaulista@yahoo.com.br>
2020-10-15 16:23:42 -0300

    Novas inserções

--- lista_exercicio3.1.cpp/exercicio10.cpp
+++ lista_exercicio3.1.cpp/exercicio10.cpp
@@ -1,3 +1,8 @@
 /* 10. Faça um programa que determine a data cronologicamente maior entre duas datas
 fornecidas pelo usuário. Cada data deve ser composta por três valores inteiros, em
 que o primeiro representa o dia, o segundo, o mês e o terceiro, o ano.*/
+
+#include <stdio.h>
+int main() {
+    
+}
--- lista_exercicio3.1.cpp/exercicio9.cpp
+++ lista_exercicio3.1.cpp/exercicio9.cpp
@@ -1,2 +1,7 @@
 /* 9. Faça um programa que mostre a data e a hora do sistema nos seguintes formatos:
 DD/MM/AAAA – mês por extenso e hora:minuto. */
+
+#include <stdio.h>
+int main() {
+    
+}
