874c44672740f3c165551085ca9887203997dc68
Robinson <robinpaulista@yahoo.com.br>
2020-10-15 17:44:02 -0300

    Primeiro commit

--- lista_exercicio3.1.cpp/exercicio18.cpp
+++ lista_exercicio3.1.cpp/exercicio18.cpp
@@ -5,6 +5,40 @@ que:
 ■ o comprimento de cada lado de um triângulo é menor que a soma dos outros dois
 lados;
 ■ chama-se equilátero o triângulo que tem três lados iguais;
-■ denomina-se isósceles o triângulo que tem o comprimento de dois lados iguais;
+■ denomina-se isóceles o triângulo que tem o comprimento de dois lados iguais;
 ■ recebe o nome de escaleno o triângulo que tem os três lados diferentes. */
 
+#include <stdio.h>
+int main(){
+    float x, y, z;
+    printf("\nInforme o valor de x: \n");
+    scanf("%f", &x);
+    printf("\nInforme o valor de y: \n");
+    scanf("%f", &y);
+    printf("\nInforme o valor de z: \n");
+    scanf("%f", &z);
+
+    if(x < y + z && y < z + x && z < x + y){
+        printf("\nTriangulo \n");
+        if(x == y || x == z){
+            printf("\nTriangulo Equilatero \n");
+            else if(x == y || y == z || z == x){
+                printf("\nTriangulo Isoceles \n");
+                    else{
+                        printf("\nTriangulo Escaleno\n");
+                    }
+                   
+                }
+            }
+        }
+    }
+     else{
+        printf("\nEsse nao e um Triangulo \n");
+    }
+    
+
+
+getchar();
+return 0;
+
+}
