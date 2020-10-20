a05a8e78cbb73ab7c4eaa1b9e63a6379414dfd6d
Robinson <robinpaulista@yahoo.com.br>
2020-10-20 11:31:54 -0300

    Lista de exercicios

--- lista_exercicio3.1.cpp/exercicio4.cpp
+++ lista_exercicio3.1.cpp/exercicio4.cpp
@@ -1,3 +1,34 @@
 /* 4. Faça um programa que receba três números e mostre-os em ordem crescente.
 Suponha que o usuário digitará três números diferentes. */
 
+#include <stdio.h>
+int main(){
+    int n1, n2, n3;
+    printf("\nDeclare três numeros: \n");
+    scanf("%d%d%d", &n1, &n2, &n3);
+    
+    if(n1 < n2 && n1 < n3){
+        if(n2 < n3)
+            printf("%d - %d - %d\n", n1, n2, n3);
+            else
+                printf("%d - %d - %d\n", n1, n2, n3);
+    }
+    if(n2 < n1 && n2 < n3){
+        if(n1<n3)
+        printf("%d - %d - %d\n", n2, n1, n3);
+        else
+        printf("%d - %d - %d\n", n2, n1, n3);
+    }
+    if(n3<n1 && n3<n2){
+        if(n1<n2)
+        printf("%d - %d - %d\n", n3, n1, n2);
+        else
+        printf("%d - %d - %d\n", n3, n1, n2);
+    }
+        
+    
+
+
+    getchar();
+    return 0;
+}
