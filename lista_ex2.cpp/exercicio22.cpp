f700be42497ba9e19479d7e0636e149f8d116026
Robinson <robinpaulista@yahoo.com.br>
2020-11-06 22:31:44 -0300

    Lista de exercícios

--- lista_ex2.cpp/exercicio22.cpp
+++ lista_ex2.cpp/exercicio22.cpp
@@ -6,14 +6,25 @@ b) o valor a ser pago por essa residência;
 c) o valor a ser pago com desconto de 15%. */
 
 #include <stdio.h>
+
 int main(){
-    float sal_minimo, quilowatts, valor_quilowatts, valor_pago, valor_desconto;
-    printf("Declare o seu salario minimo: \n");
-    scanf("%f", &sal_minimo);
+    float sal_minimo, consumo_quilowatts, valor_total, quilowatts, valor_pagar;
+    printf("Declare o valor do salario mínimo: \n");
+    scanf("%f",&sal_minimo);
+    printf("Declare a quantidade de quilowatts consumida na residência: \n");
+    scanf("%f",&consumo_quilowatts);
     
+    quilowatts = sal_minimo/5;
 
+    printf("O valor do quilowatt e: %.2f \n", quilowatts);
 
+    valor_total = consumo_quilowatts * quilowatts;
 
+    printf("O valor de quilowatts a ser pago por essa residencia e: %.2f \n", valor_total);
+    
+    valor_pagar = valor_total * 0.85;
+
+    printf("O valor a ser pago com desconto de 15%: %.2f \n", valor_pagar);
 
     getchar();
     return 0;
