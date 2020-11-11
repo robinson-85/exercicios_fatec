9936bd84dc188bcb969c322720dbb5c4dacfddbe
Robinson <robinpaulista@yahoo.com.br>
2020-11-11 11:25:29 -0300

    Lista de exercícios

--- lista_ex3.1.cpp/exercicio5.cpp
+++ lista_ex3.1.cpp/exercicio5.cpp
@@ -3,41 +3,33 @@ e um quarto número que não siga essa regra. Mostre, em seguida, os quatro núm
 em ordem decrescente. Suponha que o usuário digitará quatro números diferentes.*/
 
 #include <stdio.h>
-#include <locale.h> //Biblioteca locale � uitlizada para definar o idioma do programa.
+#include <locale.h> 
 
 int main()
 {
-    setlocale(LC_ALL,"portuguese"); //Comando utilizado para caracteres especiais aparecem.
-    int n1,n2,n3,n4;
+    setlocale(LC_ALL,"portuguese"); 
+    int num1, num2, num3, num4;
+    printf("Digite o primeiro número em ordem crescente: \n");
+    scanf("%d",&num1);
+    scanf("%d",&num2);
+    scanf("%d",&num3);
+    printf("Digite um número (fora de ordem): \n");
+    scanf("%d",&num4);
 
-    printf("N�meros em ordem decresente \n \n");
-    printf("Digite tr�s num�ros em ordem CRESENTE. \n");
+    if(num4 > num3){
+        printf("A ordem decrescente é: %d\n", num4, "-%d", num3, "-%d", num2, "-%d", num1);
+    }
+    else if(num4 > num2 && num4 < num3){
+        printf("A ordem decrescente é: %d \n", num3, "-%d", num4, "-%d", num2, "-%d", num1);
+    }
+	else if(num4 > num1 && num4 < num2){
+        printf("A ordem decrescente é: %d \n", num3, "-%d", num2, "-%d", num4, "-%d", num1);
+    }
+    else if(num4 < num1){
+        printf("A ordem decrescente é: %d \n", num3, "-%d", num2, "-%d", num1, "-%d", num4);
+    }
 
-    printf("    Digite um n�mero: ");
-        scanf("%d",&n1); // Menor numero
-    printf("    Digite um n�mero: ");
-        scanf("%d",&n2);
-    printf("    Digite um n�mero: ");
-        scanf("%d",&n3); //Maior numero
-
-    printf(" \n");
-    printf("Digite um n�mero fora da ordem: ");
-        scanf("%d",&n4);
-    printf("\n\n");
-
-    //Condi�oes
-    if ( n4 > n3){
-        printf("A ordem decresente dos n�meros �: %d,%d,%d,%d.",n4,n3,n2,n1);
-    }
-    else if  (n4 > n2){
-        printf(" ordem decresente dos n�meros �: %d,%d,%d,%d.",n3,n2,n4,n1);
-    }
-    else if  (n4 > n1){
-        printf(" ordem decresente dos n�meros �: %d,%d,%d,%d.",n3,n4,n2,n1);
-    }
-     else if  (n4 < n1){
-        printf(" ordem decresente dos n�meros �: %d,%d,%d,%d.",n4,n3,n2,n1);
-    }
+    
 
     getchar();
     return 0;
