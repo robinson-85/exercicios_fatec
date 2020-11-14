7a4a5579c16c60ebe4e8b705be2fe7747db788a8
Robinson <robinpaulista@yahoo.com.br>
2020-11-14 15:28:23 -0300

    Lista de exercícios

--- lista_ex3.1.cpp/exercicio23.cpp
+++ lista_ex3.1.cpp/exercicio23.cpp
@@ -45,7 +45,7 @@ Maior que R$ 600,00               Bem remunerado     */
 
 int main(){
     char turno_trabalho, categoria, M, V, N, O, G;
-    float sal_minimo, horas_trabalhadas, valor_aumento, novo_salario, sal_bruto, imposto, gratificacao, auxilio_alimentacao, sal_liquido;
+    float sal_minimo, horas_trabalhadas, coeficiente_salario, valor_aumento, novo_salario, sal_bruto, imposto, gratificacao, auxilio_alimentacao, sal_liquido;
     printf("Digite o valor do salario minimo: \n");
     scanf("%f", &sal_minimo);
     printf("Declare seu turno de trabalho: \nM — matutino, \nV — vespertino, \nN — noturno \n");
@@ -57,52 +57,42 @@ int main(){
     printf("\n \n");
 
     // coeficiente do salário e sobre o turno.
-   
-    if(turno_trabalho == M){
-	    valor_aumento = sal_minimo * 0.10;
-        novo_salario = sal_minimo * 1.10;
-        printf("\nValor do Aumento em 10 Porcento: R$%.2f \n", valor_aumento);
-        printf("\nValor do Novo Salario: R$%.2f \n", novo_salario);
-
+    if(turno_trabalho == 'M'){
+	    coeficiente_salario = sal_minimo * 0.10;
+        printf("\nValor da hora trabalhada: R$%.2f \n", coeficiente_salario);
     }
-    else if(turno_trabalho == V){
-        valor_aumento = sal_minimo * 0.15;
-        novo_salario = sal_minimo * 1.15;
-        printf("\nValor do Aumento em 15 Porcento: R$%.2f \n", valor_aumento);
-        printf("\nValor do Novo Salario: R$%.2f \n", novo_salario);
-
+    else if(turno_trabalho == 'V'){
+        coeficiente_salario = sal_minimo * 0.15;
+        printf("\nValor da hora trabalhada: R$%.2f \n", coeficiente_salario);
     }
-    else if(turno_trabalho == N){
-        valor_aumento = sal_minimo * 0.12;
-        novo_salario = sal_minimo * 1.12;
-        printf("\nValor do Aumento em 12 Porcento: R$%.2f \n", valor_aumento);
-        printf("\nValor do Novo Salario: R$%.2f \n", novo_salario);
-
+    else if(turno_trabalho == 'N'){
+        coeficiente_salario = sal_minimo * 0.12;
+        printf("\nValor da hora trabalhada: R$%.2f \n", coeficiente_salario);
     }
     
-    sal_bruto = horas_trabalhadas * novo_salario;
+    sal_bruto = horas_trabalhadas * coeficiente_salario;
     printf("O valor do salario bruto e: R$ %.2f\n", sal_bruto);
 
     // imposto sobre o salário e a categoria.
     
-    if(categoria == O && sal_bruto >=300){
-	    imposto = sal_bruto - (5/100);
+    if(categoria == 'O' && sal_bruto >=300){
+	    imposto = sal_bruto * 0.05;
     }
-    else if(categoria == O && sal_bruto <300){
-	    imposto = sal_bruto - (3/100);
+    else if(categoria == 'O' && sal_bruto <300){
+	    imposto = sal_bruto * 0.03;
     }
-    else if(categoria == G && sal_bruto >=400){
-	    imposto = sal_bruto - (6/100);
+    else if(categoria == 'G' && sal_bruto >=400){
+	    imposto = sal_bruto * 0.06;
     }
-    else if(categoria == G && sal_bruto <400){
-	    imposto = sal_bruto - (4/100);
+    else if(categoria == 'G' && sal_bruto <400){
+	    imposto = sal_bruto * 0.04;
     }
     
     printf("O imposto sobre o salário bruto será: R$ %.2f \n", imposto);
 
     // gratificação
 
-    if(turno_trabalho == N && horas_trabalhadas>80){
+    if(turno_trabalho == 'N' && horas_trabalhadas>80){
 	    gratificacao = sal_bruto + 50;
     }
     else{
@@ -112,7 +102,7 @@ int main(){
 
     // auxílio-alimentação
     
-    if(categoria == O && novo_salario <=25){
+    if(categoria == 'O' && novo_salario <=25){
         auxilio_alimentacao =  sal_bruto/3;
     }
     else{
