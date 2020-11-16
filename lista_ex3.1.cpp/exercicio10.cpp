2c95cc382ce51f7deb7d4bbdac5c8124e5ec0407
Robinson <robinpaulista@yahoo.com.br>
2020-11-16 14:40:06 -0300

    Lista de exercícios

--- lista_ex3.1.cpp/exercicio10.cpp
+++ lista_ex3.1.cpp/exercicio10.cpp
@@ -2,5 +2,49 @@
 fornecidas pelo usuário. Cada data deve ser composta por três valores inteiros, em
 que o primeiro representa o dia, o segundo, o mês e o terceiro, o ano.*/
 
+#include <stdio.h>
 
+int main(){
+    int maior, menor, dia1, dia2, mes1, mes2, ano1, ano2;
+    printf("Declare o dia: \n");
+    scanf("%d", &dia1);
+    printf("Declare um mês: \n");
+    scanf("%d", &mes1);
+    printf("Declare um ano: \n");
+    scanf("%d", &ano1);
+    printf("Declare o dia: \n");
+    scanf("%d", &dia2);
+    printf("Declare um mes: \n");
+    scanf("%d", &mes2);
+    printf("Declare um ano: \n");
+    scanf("%d", &ano2);
+    
+    printf("A primeira data é: %d/%d/%d \n", dia1, mes1, ano1);
+    printf("A segunda data é: %d/%d/%d \n", dia2, mes2, ano2);
+    
+    if(ano1>ano2){
+    printf("A maior data é: %d/%d/%d \n", dia1, mes1, ano1);
+    }
+    else if(ano2>ano1){
+    printf("A maior data é: %d/%d/%d \n", dia2, mes2, ano2);
+    }
+    else if(mes1>mes2){
+    printf("A maior data é: %d/%d/%d \n", dia1, mes1, ano1);
+    }
+    else if(mes2>mes1){
+    printf("A maior data é: %d/%d/%d \n", dia2, mes2, ano2);
+    }
+    else if(dia1>dia2){
+    printf("A maior data é: %d/%d/%d \n", dia1, mes1, ano1);
+    }
+    else if(dia2>dia1){
+    printf("A maior data é: %d/%d/%d \n", dia2, mes2, ano2);
+    }
+    else{
+        printf("As datas são iguais! \n");
+    }
+
+    getchar();
+    return 0;
+}
 
--- lista_ex3.1.cpp/exercicio11.cpp
+++ lista_ex3.1.cpp/exercicio11.cpp
@@ -3,3 +3,11 @@
 duração do jogo (horas e minutos), sabendo que o tempo máximo de duração do jogo
 é de 24 horas e que ele pode começar em um dia e terminar no dia seguinte.*/
 
+#include<stdio.h>
+
+int main(){
+
+
+    getchar();
+    return 0;
+}
