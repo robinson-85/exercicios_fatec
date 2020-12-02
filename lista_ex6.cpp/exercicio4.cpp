64ec9bc5d24de845e96af30beecd51391087c3a9
Robinson <robinpaulista85@gmail.com>
2020-12-02 19:30:57 -0300

    Lista de exercícios

--- lista_ex6.cpp/exercicio4.cpp
+++ lista_ex6.cpp/exercicio4.cpp
@@ -1,3 +1,12 @@
 /* 4. Criar 2 vetores com 8 elementos, preencher o primeiro com valores, a seguir
 preencha o 2o com os valores do primeiro em ordem inversa. */
 
+#include <stdio.h>
+int main(){
+    int vetor1[8] = 
+
+
+
+    getchar();
+    return 0;
+}
