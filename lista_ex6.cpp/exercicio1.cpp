5486ca3a75b3fb75e16f996ad0ace7bc3b20505f
Robinson <robinpaulista85@gmail.com>
2020-12-04 10:52:31 -0300

    Lista de exercícios

--- lista_ex6.cpp/exercicio1.cpp
+++ lista_ex6.cpp/exercicio1.cpp
@@ -2,18 +2,21 @@
 
 #include <stdio.h>
 int main(){
-    int vetor[10] = {};
+    int vetor[10];
+    int i;
     
         for(int i = 0; i < 10; i++){
-            printf("Digite um valor: \n");
-            scanf("%d \n", &vetor[i]);
+            printf("Digite a nota: \n");
+            scanf("%d", &vetor[i]);
         }
 
-        for(int i = 0; i >= 7; i++){
-            printf("%d - \n", vetor[i]);
+        for(int i = 0; i < 10; i++){
+            if(i>=7){
+                printf("As notas maiores que 7 são: %d\n", i);
+            }
         }
 
-        printf("As notas maiores que 7 são: %d\n", vetor[i]);
+        
 
         getchar();
         return 0;
